c5aebde822fb4c6d3937d9337ce3895ac5f408cb
Shigosag (Segun Arulogun Gabriel) <88502810+Shigosag@users.noreply.github.com>
2026-09-03 15:00:09 +0000

    Update posRoutes.ts

--- backend/src/routes/posRoutes.ts
+++ backend/src/routes/posRoutes.ts
@@ -3,4 +3,5 @@ import { POSController } from "../controllers/posController.js";
 const router = Router();
 router.post("/verify-account", POSController.verifyAccountNumber);
 router.post("/process-transfer", POSController.processTransfer);
+router.get("/transactions", POSController.getTransactions);
 export default router;
