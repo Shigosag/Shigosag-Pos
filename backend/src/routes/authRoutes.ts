c0d9452ad75d96356c1a290dcb74815c7947a904
Shigosag (Segun Arulogun Gabriel) <88502810+Shigosag@users.noreply.github.com>
2026-09-06 10:54:24 +0000

    Update authRoutes.ts

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -3,9 +3,10 @@ import { AuthController } from "../controllers/authController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = Router();
+
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
-
+router.get("/profile", authMiddleware, AuthController.getProfile);
 router.delete("/delete-account", authMiddleware, AuthController.deleteAccount);
 
 export default router;
